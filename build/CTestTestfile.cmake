2050654d1a8157a039a891a9f24606e138509563
cona <ytam1208@gmail.com>
2020-06-19 23:18:15 +0900

    0619

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cona/algorithm_ws/src
-# Build directory: /home/cona/algorithm_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(Backjun)
-subdirs(C_function)
