e2c6fb83c81126c77cebe3ed6bc8717332e920d9
cona <ytam1208@gmail.com>
2020-07-26 13:48:12 +0900

    opencv_pkg_add

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/cona/algorithm_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/cona/cona_msgs/devel;/opt/ros/kinetic".split(';') if "/home/cona/cona_msgs/devel;/opt/ros/kinetic" != "" else []
