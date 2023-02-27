b5045593e6862700d41432c61c4b73bee9bb8dca
ytam1208 <ytam1208@gmail.com>
2023-02-27 13:39:55 +0900

    VSLAM study

--- ROS_build/src/Opencv/src/Vision/Epipolar_geometry.cpp
+++ ROS_build/src/Opencv/src/Vision/Epipolar_geometry.cpp
@@ -3,93 +3,93 @@
 #include <Opencv/Opencv_tuto.hpp>
 #include <Opencv/read_path.hpp>
 
-class Get_data
-{
-    private:
-        Path::Loader get_path;
-        Display::Lodar check_mat;
-        std::string L_path;
-        std::string R_path;
-        cv::Mat L_src, R_src;
+// class Get_data
+// {
+//     private:
+//         Path::Loader get_path;
+//         Display::Lodar check_mat;
+//         std::string L_path;
+//         std::string R_path;
+//         cv::Mat L_src, R_src;
 
-    public:
-        bool operator()(cv::Mat& inputL_mat, cv::Mat& inputR_mat){
+//     public:
+//         bool operator()(cv::Mat& inputL_mat, cv::Mat& inputR_mat){
 
-            inputL_mat = L_src;
-            inputR_mat = R_src;
-            return 1;
-        }
-        Get_data(){
-            L_path = get_path("mac") + "aloeL.jpg";
-            R_path = get_path("mac") + "aloeR.jpg";
-            L_src = cv::imread(L_path, cv::IMREAD_GRAYSCALE);
-            R_src = cv::imread(R_path, cv::IMREAD_GRAYSCALE);
+//             inputL_mat = L_src;
+//             inputR_mat = R_src;
+//             return 1;
+//         }
+//         Get_data(){
+//             L_path = get_path("mac") + "aloeL.jpg";
+//             R_path = get_path("mac") + "aloeR.jpg";
+//             L_src = cv::imread(L_path, cv::IMREAD_GRAYSCALE);
+//             R_src = cv::imread(R_path, cv::IMREAD_GRAYSCALE);
 
-            if(check_mat(L_src) && check_mat(R_src)){
-                // cv::imshow("aloeL", L_src);
-                // cv::imshow("aloeR", R_src);
-                // cv::waitKey(0);
-            }
-            else    
-                std::cout << "No img" << std::endl;
-        }
-        ~Get_data(){}
-};
+//             if(check_mat(L_src) && check_mat(R_src)){
+//                 // cv::imshow("aloeL", L_src);
+//                 // cv::imshow("aloeR", R_src);
+//                 // cv::waitKey(0);
+//             }
+//             else    
+//                 std::cout << "No img" << std::endl;
+//         }
+//         ~Get_data(){}
+// };
 
-class ORB_feature
-{
-    private:
-        cv::Mat key_L, key_R;
-        cv::Mat L_ptr, R_ptr; 
-        std::vector<cv::KeyPoint> L_point, R_point;
+// class ORB_feature
+// {
+//     private:
+//         cv::Mat key_L, key_R;
+//         cv::Mat L_ptr, R_ptr; 
+//         std::vector<cv::KeyPoint> L_point, R_point;
 
-        void extract_ORB_feature(){
-            cv::imshow("aloeL", L_ptr);
-            cv::imshow("aloeR", R_ptr);
-            cv::waitKey(0);
+//         void extract_ORB_feature(){
+//             cv::imshow("aloeL", L_ptr);
+//             cv::imshow("aloeR", R_ptr);
+//             cv::waitKey(0);
 
-            cv::Ptr<cv::Feature2D> orb = cv::ORB::create(); // const static auto&  
-            orb->detectAndCompute(L_ptr, cv::noArray(), L_point, key_L);
-            orb->detectAndCompute(R_ptr, cv::noArray(), R_point, key_R);
-        };
-        void matches_ORB_feature(){
-            cv::Ptr<cv::DescriptorMatcher> matcher = cv::BFMatcher::create(cv::NORM_HAMMING);
-            std::vector<cv::DMatch> matches;
-            matcher->match(key_L, key_R, matches);
-            std::sort(matches.begin(), matches.end());
-            std::vector<cv::DMatch> good_matches(matches.begin(), matches.begin() + 100);
+//             cv::Ptr<cv::Feature2D> orb = cv::ORB::create(); // const static auto&  
+//             orb->detectAndCompute(L_ptr, cv::noArray(), L_point, key_L);
+//             orb->detectAndCompute(R_ptr, cv::noArray(), R_point, key_R);
+//         };
+//         void matches_ORB_feature(){
+//             cv::Ptr<cv::DescriptorMatcher> matcher = cv::BFMatcher::create(cv::NORM_HAMMING);
+//             std::vector<cv::DMatch> matches;
+//             matcher->match(key_L, key_R, matches);
+//             std::sort(matches.begin(), matches.end());
+//             std::vector<cv::DMatch> good_matches(matches.begin(), matches.begin() + 100);
 
-            cv::Mat dst;
-            cv::drawMatches(L_ptr, L_point, R_ptr, R_point, good_matches, dst, cv::Scalar::all(-1), cv::Scalar::all(-1), std::vector<char>());
-            cv::drawKeypoints(L_ptr, L_point, L_ptr, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-            cv::drawKeypoints(R_ptr, R_point, R_ptr, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+//             cv::Mat dst;
+//             cv::drawMatches(L_ptr, L_point, R_ptr, R_point, good_matches, dst, cv::Scalar::all(-1), cv::Scalar::all(-1), std::vector<char>());
+//             cv::drawKeypoints(L_ptr, L_point, L_ptr, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+//             cv::drawKeypoints(R_ptr, R_point, R_ptr, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
-            cv::imshow("dst", dst);
-            cv::imshow("L_ptr", L_ptr);
-            cv::imshow("R_ptr", R_ptr);
+//             cv::imshow("dst", dst);
+//             cv::imshow("L_ptr", L_ptr);
+//             cv::imshow("R_ptr", R_ptr);
 
-            cv::Mat fundamental_matrix = cv::findFundamentalMat(L_point, R_point, FM_8POINT);
-            for(auto iter : L_point){
+//             cv::Mat fundamental_matrix = cv::findFundamentalMat(L_point, R_point, FM_8POINT);
+//             for(auto iter : L_point){
 
-                cv::computeCorrespondEpilines(iter.pt, 1, fundamental_matrix);
+//                 cv::computeCorrespondEpilines(iter.pt, 1, fundamental_matrix);
 
-            }
+//             }
 
-            cv::waitKey(0);
-        }
-    public:
-        ORB_feature(Get_data gd){
-            gd(L_ptr, R_ptr);
-            extract_ORB_feature();
-            matches_ORB_feature();
-        }
-        ~ORB_feature(){}
-};
+//             cv::waitKey(0);
+//         }
+//     public:
+//         ORB_feature(Get_data gd){
+//             gd(L_ptr, R_ptr);
+//             extract_ORB_feature();
+//             matches_ORB_feature();
+//         }
+//         ~ORB_feature(){}
+// };
 
 int main()
 {
-    Get_data gd;
-    ORB_feature of(gd);
-
-    return 0;
+    //Get_data gd;
+    //ORB_feature of(gd);
+    std::cout << "hello " << std::endl;
+    return (0);
 }
---
+++ ROS_build/src/Opencv/src/Vision/stereo_vision.cpp
@@ -0,0 +1,95 @@
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <string>
+#include <Eigen/Core>
+#include <pangolin/pangolin.h>
+#include <unistd.h>
+
+using namespace std;
+using namespace Eigen;
+
+string left_file = "/Users/yeontaemin/github/algorithm_ws/ROS_build/src/Opencv/data/aloeL.jpg";
+string right_file = "/Users/yeontaemin/github/algorithm_ws/ROS_build/src/Opencv/data/aloeR.jpg";
+
+void showPointCloud(
+    const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &pointcloud);
+
+int main(int argc, char **argv) {
+
+    // 카메라 내부 파라미터 
+    double fx = 718.856, fy = 718.856, cx = 607.1928, cy = 185.2157;
+    // 베이스 링크
+    double b = 0.573;
+
+    // 이미지 읽기
+    cv::Mat left = cv::imread(left_file, 0);
+    cv::Mat right = cv::imread(right_file, 0);
+    cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(
+        0, 96, 9, 8 * 9 * 9, 32 * 9 * 9, 1, 63, 10, 100, 32);    //매개변수
+    cv::Mat disparity_sgbm, disparity;
+    sgbm->compute(left, right, disparity_sgbm);
+    disparity_sgbm.convertTo(disparity, CV_32F, 1.0 / 16.0f);
+
+    vector<Vector4d, Eigen::aligned_allocator<Vector4d>> pointcloud;
+
+    for (int v = 0; v < left.rows; v++)
+        for (int u = 0; u < left.cols; u++) {
+            if (disparity.at<float>(v, u) <= 0.0 || disparity.at<float>(v, u) >= 96.0) continue;
+
+            Vector4d point(0, 0, 0, left.at<uchar>(v, u) / 255.0); 
+
+            double x = (u - cx) / fx;
+            double y = (v - cy) / fy;
+            double depth = fx * b / (disparity.at<float>(v, u));
+            point[0] = x * depth;
+            point[1] = y * depth;
+            point[2] = depth;
+
+            pointcloud.push_back(point);
+        }
+
+    cv::imshow("disparity", disparity / 96.0);
+    cv::waitKey(0);
+    showPointCloud(pointcloud);
+    return 0;
+}
+
+void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &pointcloud) {
+
+    if (pointcloud.empty()) {
+        cerr << "Point cloud is empty!" << endl;
+        return;
+    }
+
+    pangolin::CreateWindowAndBind("Point Cloud Viewer", 1024, 768);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    pangolin::OpenGlRenderState s_cam(
+        pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
+        pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0)
+    );
+
+    pangolin::View &d_cam = pangolin::CreateDisplay()
+        .SetBounds(0.0, 1.0, pangolin::Attach::Pix(175), 1.0, -1024.0f / 768.0f)
+        .SetHandler(new pangolin::Handler3D(s_cam));
+
+    while (pangolin::ShouldQuit() == false) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        d_cam.Activate(s_cam);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+        glPointSize(2);
+        glBegin(GL_POINTS);
+        for (auto &p: pointcloud) {
+            glColor3f(p[3], p[3], p[3]);
+            glVertex3d(p[0], p[1], p[2]);
+        }
+        glEnd();
+        pangolin::FinishFrame();
+        usleep(1000);   // sleep 5 ms
+    }
+    return;
+}
